3cbd9de18d9864f2fbbdd032ced168480857b18a
Tony Lea <tnylea@gmail.com>
2024-05-22 20:00:47 -0400

    Adding updated environment files

--- laravel.sh
+++ laravel.sh
@@ -28,5 +28,12 @@ php artisan vendor:publish --tag=auth:migrations
 # Run the migrations
 php artisan migrate
 
+# Replace the User model content with the new inheritance from Devdojo\Auth\Models\User
+sed -i '' 's/Illuminate\\Foundation\\Auth\\User as Authenticatable/Devdojo\\Auth\\Models\\User as AuthUser/' app/Models/User.php
+sed -i '' 's/class User extends Authenticatable/class User extends AuthUser/' app/Models/User.php
+
+sed -i '' 's/MAIL_MAILER=log/MAIL_MAILER=smtp/' .env
+sed -i '' 's/MAIL_USERNAME="Laravel"/MAIL_USERNAME=null/' .env
+
 # Startup the server on localhost
 php artisan serve
